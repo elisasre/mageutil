eca02451ad6cd78ac70a3c0c99a607a984000e22
Janne Mensonen <janne.mensonen@elisa.fi>
2024-04-15 10:48:54 +0300

    add build all target for lambdas

---
+++ lambda/target/lambda.go
@@ -0,0 +1,24 @@
+// Package target exposes Lambda targets that can be imported in magefile using [import syntax].
+// When using this package the user has to set target.BuildTargets.
+//
+// [import syntax]: https://magefile.org/importing/
+package target
+
+import (
+	"context"
+
+	"github.com/elisasre/mageutil/lambda"
+	"github.com/magefile/mage/mg"
+)
+
+var (
+	BuildTargets = []string{}
+	GOARCH       = "amd64"
+)
+
+type Lambda mg.Namespace
+
+// BuildAll builds lambda bootstrap binaries and calculates sha sums for them.
+func (Lambda) BuildAll(ctx context.Context) error {
+	return lambda.BuildAll(ctx, BuildTargets, GOARCH)
+}
