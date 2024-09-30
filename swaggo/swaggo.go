4f5d7c1e84e48ef79b027e16f8350104946224d4
Rolf Ahrenberg <rolf.ahrenberg@elisa.fi>
2024-09-30 09:29:16 +0300

    Add OpenAPIAndVerify target

--- swaggo/swaggo.go
+++ swaggo/swaggo.go
@@ -3,7 +3,9 @@ package swaggo
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/elisasre/mageutil/git"
 	"github.com/swaggo/swag"
 	"github.com/swaggo/swag/gen"
 )
@@ -31,4 +33,16 @@ func GenerateDocs(ctx context.Context, searchDir, apiFile, outputDir string) err
 // GenerateDocsWithConf generates the OpenAPI docs with given config.
 func GenerateDocsWithConf(ctx context.Context, conf gen.Config) error {
 	return gen.New().Build(&conf)
+}
+
+// GenerateAndVerify generates the OpenAPI docs with sane default config and and verifies that there are no changes to output files.
+// This is useful in CI/CD pipelines to validate that OpenAPI docs are up to date.
+func GenerateDocsAndVerify(ctx context.Context, searchDir, apiFile, outputDir string) error {
+	if err := GenerateDocs(ctx, searchDir, apiFile, outputDir); err != nil {
+		return err
+	}
+	if err := git.DiffFilesWithExit(ctx, outputDir); err != nil {
+		return fmt.Errorf("%s is not in sync with the version control", outputDir)
+	}
+	return nil
 }
