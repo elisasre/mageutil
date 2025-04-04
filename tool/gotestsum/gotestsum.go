e2ef4679599babded3186be199ad526e022a25bf
Henri Koski <henri.t.koski@gmail.com>
2025-04-04 12:49:44 +0300

    Add gotestsum default test cmd

--- tool/gotestsum/gotestsum.go
+++ tool/gotestsum/gotestsum.go
@@ -2,9 +2,16 @@
 package gotestsum
 
 import (
+	"context"
+
+	"github.com/elisasre/mageutil/golang"
 	"github.com/elisasre/mageutil/tool"
 )
 
 const ToolName = "gotest.tools/gotestsum"
 
 var Tool = tool.New(ToolName)
+
+var DefaultTestCmd golang.Cmd = func(ctx context.Context, env map[string]string, args ...string) error {
+	return Tool.ExecWith(ctx, env, append([]string{"--"}, args...)...)
+}
